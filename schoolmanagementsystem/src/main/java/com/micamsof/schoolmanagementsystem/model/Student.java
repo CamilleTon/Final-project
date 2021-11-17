cf49d68cb425952df5d2f4898eb98e60c051a42a
Mitracles <michaelmitra45@gmail.com>
2021-11-17 17:16:16 +0800

    initial commit

---
+++ schoolmanagementsystem/src/main/java/com/micamsof/schoolmanagementsystem/model/Student.java
@@ -0,0 +1,13 @@
+package com.micamsof.schoolmanagementsystem.model;
+
+import java.io.Serializable;
+
+public class Student implements Serializable {
+    private Long id;
+    private String name;
+    private String email;
+    private String section;
+    private String contact;
+    private String imageUrl;
+    private String studentNumber;
+}
